bb986f8120784e60bdeeb6a8121114f9bf4bc683
AliceHjxxxx <775326703@qq.com>
2021-06-18 17:19:36 +0800

    项目初始化-目录初始化结构调整

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,31 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    root: true,
+    env: {
+        node: true
+    },
+    extends: ["plugin:vue/essential", "@vue/standard"],
+    parserOptions: {
+        parser: "babel-eslint"
+    },
+    rules: {
+        "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+        quotes: "off",
+        semi: "off",
+        "comma-dangle": "off",
+        "no-empty": "off",
+        "no-regex-spaces": "off",
+        indent: "off",
+        "eol-last": "off",
+        "func-call-spacing": "off",
+        "spaced-comment": "off",
+        "no-trailing-spaces": "off",
+        "space-before-blocks": "off",
+        "space-before-blocks": "off",
+        "quote-props": "off",
+        "no-useless-return": "off",
+        "no-unused-vars": "off",
+        "prefer-const": "off",
+        "space-in-parens": "off"
+    }
 }
